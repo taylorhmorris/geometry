f6ba14dfed45b1a2f5f24393b386d520734816e5
tmorris <zq8fznrj@duck.com>
2023-04-08 18:49:48 +0200

    chore: compile package for commonjs and esnext

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,5 @@
-import { Rect as RectClass } from './Rect.class';
-import { Vector as VectorClass } from './Vector.class';
-import { Circle as CircleClass } from './Circle.class';
-import { Face as FaceEnum } from './Face.enum';
-import { PointArray as PointArrayType } from './PointArray.type';
-
-/**
- * {@link PointArray}
- */
-export type PointArray = PointArrayType;
-
-/**
- * {@link Rect}
- */
-export const Rect = RectClass;
-/**
- * {@link Rect}
- */
-export type Rect = RectClass;
-
-/**
- * {@link Vector}
- */
-export const Vector = VectorClass;
-/**
- * {@link Vector}
- */
-export type Vector = VectorClass;
-
-/**
- * {@link Circle}
- */
-export const Circle = CircleClass;
-export type Circle = CircleClass;
-
-/**
- * {@link Face}
- */
-export const Face = FaceEnum;
-export type Face = FaceEnum;
-
-export default { Rect: Rect, Vector: Vector, Circle: Circle, Face: Face };
+export { Rect } from './Rect.class';
+export { Vector } from './Vector.class';
+export { Circle } from './Circle.class';
+export { Face } from './Face.enum';
+export { PointArray } from './PointArray.type';
