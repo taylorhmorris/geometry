cc29603c853f7d169927cd16eff2b7ea23176401
taylorhmorris <18409399+taylorhmorris@users.noreply.github.com>
2025-04-08 13:41:07 -0500

    feat: add Polygon to index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { Face } from './Face.enum';
 export { Point } from './Point.class';
 export { Rect } from './Rect.class';
 export { Vector } from './Vector.class';
+export { Polygon } from './Polygon.class';
 
 export type { PointArray } from './PointArray.type';
 
